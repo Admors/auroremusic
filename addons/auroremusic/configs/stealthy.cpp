2a9cca4508ba13e76f95f163b69c7652625b83b6
Admors <tthejuju@gmail.com>
2023-07-03 19:35:03 +0200

    Aurore 0.0.1

---
+++ addons/auroremusic/configs/stealthy.cpp
@@ -0,0 +1,7 @@
+    class AMP_STEALTHY_Music_1
+    {
+        name = "Improvised Entry SWRC";
+        sound[] = {"auroremusic\amp_ogg\stealthy\Improvised_Entry_SWRC.ogg", 1, 1};
+        duration = 93;
+        musicClass = "AMP_STEALTHY";
+    };

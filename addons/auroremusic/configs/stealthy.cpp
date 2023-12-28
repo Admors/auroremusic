937ea26ae5a1ca32893200da8e80349b644b256f
Admors <admors2309@gmail.com>
2023-12-28 19:45:29 +0100

    Aurore - Fix

--- addons/auroremusic/configs/stealthy.cpp
+++ addons/auroremusic/configs/stealthy.cpp
@@ -6,14 +6,14 @@
     Description: Merci de ne pas {Copier,Modifier,Plagier} le contenue suivant sans l'autorisation au préalable de l'Administrateur en charge.       
 */
 
-MUSIC(STEALTHY_Music_1, "Improvised Entry SWRC", STEALTHY, "auroremusic\\amp_ogg\\stealthy\\Improvised_Entry_SWRC.ogg", 93)
+MUSIC(STEALTHY_Music_1, "Improvised Entry SWRC", STEALTHY, "auroremusic\amp_ogg\stealthy\Improvised_Entry_SWRC.ogg", 93)
 
-MUSIC(STEALTHY_Music_2, "Jawas Attack", STEALTHY, "auroremusic\\amp_ogg\\stealthy\\Jawas_Attack.ogg", 227)
+MUSIC(STEALTHY_Music_2, "Jawas Attack", STEALTHY, "auroremusic\amp_ogg\stealthy\Jawas_Attack.ogg", 227)
 
-MUSIC(STEALTHY_Music_3, "Sneaking Under the Shield", STEALTHY, "auroremusic\\amp_ogg\\stealthy\\Sneaking.ogg", 265)
+MUSIC(STEALTHY_Music_3, "Sneaking Under the Shield", STEALTHY, "auroremusic\amp_ogg\stealthy\Sneaking.ogg", 265)
 
-MUSIC(STEALTHY_Music_4, "Infiltrating Ziro's Lair", STEALTHY, "auroremusic\\amp_ogg\\stealthy\\Infiltrating_Ziro_Lair.ogg", 142)
+MUSIC(STEALTHY_Music_4, "Infiltrating Ziro's Lair", STEALTHY, "auroremusic\amp_ogg\stealthy\Infiltrating_Ziro_Lair.ogg", 142)
 
-MUSIC(STEALTHY_Music_5, "Zam the Assassin and the Chase Throuh Coruscant", STEALTHY, "auroremusic\\amp_ogg\\stealthy\\Zam_the_Assassin_and_the_Chase_Through_Coruscant.ogg", 667)
+MUSIC(STEALTHY_Music_5, "Zam the Assassin and the Chase Throuh Coruscant", STEALTHY, "auroremusic\amp_ogg\stealthy\Zam_the_Assassin_and_the_Chase_Through_Coruscant.ogg", 667)
 
-MUSIC(STEALTHY_Music_6, "Second Thoughts", STEALTHY, "auroremusic\\amp_ogg\\stealthy\\Second.ogg", 260)
+MUSIC(STEALTHY_Music_6, "Second Thoughts", STEALTHY, "auroremusic\amp_ogg\stealthy\Second.ogg", 260)
